613e4d35523c9635fc7e9c4ed46e51b86c8f638f
Raghav S <20samavedam_raghav@dublinstudents.net>
2020-06-22 12:33:52 -0400

    cleaned up auxillary function and added an option to generate a random path from one side of the grid to another

--- pathGenerator.py
+++ pathGenerator.py
@@ -2,13 +2,14 @@
 import random, math
 from operator import itemgetter
 
-def sort_by_magnitude(current_position, displacement_vectors):
-    #sorts list of moves in increasing order of the magnitude of the position gained from applying the move to the current position
+def sort_by_distance_to_end(current_position, displacement_vectors, ending_position):
+    #sorts list of moves in increasing order of the distance to the ending position of the position gained from applying the move to the current position
     move_magnitude = []
     for move in displacement_vectors:
         position = (current_position[0] + move[0], current_position[1] + move[1])
-        position_magnitude = math.sqrt(position[0]**2 + position[1]**2)
-        move_magnitude.append((move, position_magnitude))
+        distance_vector = (ending_position[0] - position[0], ending_position[1] - position[1])
+        distance_to_end = math.sqrt(distance_vector[0]**2 + distance_vector[1]**2)
+        move_magnitude.append((move, distance_to_end))
     sorted_moves_plus_mag = sorted(move_magnitude, key=itemgetter(1))
     return [x[0] for x in sorted_moves_plus_mag]
 
@@ -25,7 +26,7 @@ def generate_chase_points(scale, prob_distribution):
     #the last entry of prob_distribution refers to to the probability the chasing boat disappears from view
 
     #returned values:
-    #list of points of length "scale" + 1
+    #list of points of length "scale"
 
     starting_side = random.choice(["up", "down", "left", "right"])
     if starting_side == "up":
@@ -47,7 +48,70 @@ def generate_chase_points(scale, prob_distribution):
     #we now create the trajectory
     trajectory = [starting_pos]
     for i in range(scale):
-        sorted_moves = sort_by_magnitude(current_pos, possible_moves)
+        sorted_moves = sort_by_distance_to_end(current_pos, possible_moves, (0, 0))
+
+        weighted_moves = [] #we randomly select our next move from here
+        for i in range(len(sorted_moves)):
+            for j in range(prob_distribution[i]):
+                weighted_moves.append(sorted_moves[i])
+        move = random.choice(weighted_moves)
+        current_pos[0] += move[0]
+        current_pos[1] += move[1]
+
+        #now determine if the "chasing boat" is visible
+        rand_number = random.random() #random number between 0 and 1
+        if rand_number > (prob_distribution[9]/100):
+            trajectory.append((current_pos[0], current_pos[1]))
+        else:
+            trajectory.append(("invisible", "invisible"))
+
+    return trajectory
+
+def generate_random_path_points(scale, prob_distribution):
+    #method objective:
+    #return a list of points of a boat moving from side to another of the grid with given erraticity
+
+    #parameter descriptions:
+    #grid across which the chase is happening will have dimension (2*scale) x (2*scale), scale must be > 0
+    #the observing boat will always have position (0, 0) in this grid
+    #prob_distribution is a list of ten non-negative integers, the first 9 sum up to 100, the last entry is less than 100
+    #the 1st entry in prob_distribution refers to the probability the chasing boat moves to the grid position that is closest to its target destination, the 2nd entry refers to the probability the chasing boat moves towards the 2nd closest grid position that is the 2nd closest to its target destination and so on...
+    #the last entry of prob_distribution refers to to the probability the moving boatdisappears from view
+
+    #returned values:
+    #list of points of length 2*"scale"
+
+    starting_side = random.choice(["up", "down", "left", "right"])
+    if starting_side == "up":
+        starting_pos = (random.choice([x for x in range(-1*scale, scale + 1)]), scale)
+    if starting_side == "down":
+        starting_pos = (random.choice([x for x in range(-1*scale, scale + 1)]), -1*scale)
+    if starting_side == "left":
+        starting_pos = (-1*scale, random.choice([x for x in range(-1*scale, scale + 1)]))
+    if starting_side == "right":
+        starting_pos = (scale, random.choice([x for x in range(-1*scale, scale + 1)]))
+
+    ending_side = random.choice(["up", "down", "left", "right"])
+    if ending_side == "up":
+        ending_pos = (random.choice([x for x in range(-1*scale, scale + 1)]), scale)
+    if ending_side == "down":
+        ending_pos = (random.choice([x for x in range(-1*scale, scale + 1)]), -1*scale)
+    if ending_side == "left":
+        ending_pos = (-1*scale, random.choice([x for x in range(-1*scale, scale + 1)]))
+    if ending_side == "right":
+        ending_pos = (scale, random.choice([x for x in range(-1*scale, scale + 1)]))
+
+    list_of_positions = []
+    list_of_positions.append(starting_pos)
+
+    current_pos = [starting_pos[0], starting_pos[1]]
+
+    possible_moves = [(0, 0), (0, 1), (0, -1), (1, 0), (1, 1), (1, -1), (-1, 0), (-1, 1), (-1, -1)] #displacement vectors
+
+    #we now create the trajectory
+    trajectory = [starting_pos]
+    for i in range(2*scale):
+        sorted_moves = sort_by_distance_to_end(current_pos, possible_moves, ending_pos)
 
         weighted_moves = [] #we randomly select our next move from here
         for i in range(len(sorted_moves)):
@@ -68,5 +132,5 @@ def generate_chase_points(scale, prob_distribution):
 
 
 if __name__ == "__main__":
-    points = generate_chase_points(10, (100, 0, 0, 0, 0, 0, 0, 0, 0, 0)) #example 
+    points = generate_chase_points(10, (100, 0, 0, 0, 0, 0, 0, 0, 0, 0)) #example
     print(points)
