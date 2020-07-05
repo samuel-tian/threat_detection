62d24ae4ed4ecfc262e8d6154261ae1ee6193fad
Samuel <samuel.tian2003@gmail.com>
2020-07-04 21:58:10 -0500

    mpfr integration

--- HMMClasses/hmmc/hmm.h
+++ HMMClasses/hmmc/hmm.h
@@ -18,27 +18,27 @@ class HMM {
 public:
 
 	int N, K; // number of states, number of observations
-	std::vector<mpf_class> init_prob; // siez N vector, initial state probabilities
-	std::vector<std::vector<mpf_class> > trans_prob; // N x N matrix, probability of transitioning from state i to state j
-	std::vector<std::vector<mpf_class> > emit_prob; // N x K matrix, probability of emitting symbol j at state i
+	std::vector<mpfr::mpreal> init_prob; // siez N vector, initial state probabilities
+	std::vector<std::vector<mpfr::mpreal> > trans_prob; // N x N matrix, probability of transitioning from state i to state j
+	std::vector<std::vector<mpfr::mpreal> > emit_prob; // N x K matrix, probability of emitting symbol j at state i
 
 	HMM() = default;
 
 	HMM(int num_states, int num_emissions);
 
-	HMM(std::vector<std::vector<mpf_class> >& emit_prob);
+	HMM(std::vector<std::vector<mpfr::mpreal> >& emit_prob);
 
 	friend std::ostream& operator << (std::ostream& os, const HMM& hmm);
 
-	std::pair<std::vector<std::vector<mpf_class> >, std::vector<mpf_class> > generate_forwards(std::vector<int>& obs_seq);
+	std::pair<std::vector<std::vector<mpfr::mpreal> >, std::vector<mpfr::mpreal> > generate_forwards(std::vector<int>& obs_seq);
 
-	std::vector<std::vector<mpf_class> > generate_backwards(std::vector<int>& obs_seq, std::vector<mpf_class>& scale_factors);
+	std::vector<std::vector<mpfr::mpreal> > generate_backwards(std::vector<int>& obs_seq, std::vector<mpfr::mpreal>& scale_factors);
 
-	mpf_class evaluate(std::vector<int>& obs_seq);
+	mpfr::mpreal evaluate(std::vector<int>& obs_seq);
 
-	std::vector<std::vector<mpf_class> > generate_gamma(std::vector<int>& obs_seq, std::vector<std::vector<mpf_class> >& alpha, std::vector<std::vector<mpf_class> >& beta, std::vector<mpf_class>& scale_factors);
+	std::vector<std::vector<mpfr::mpreal> > generate_gamma(std::vector<int>& obs_seq, std::vector<std::vector<mpfr::mpreal> >& alpha, std::vector<std::vector<mpfr::mpreal> >& beta, std::vector<mpfr::mpreal>& scale_factors);
 
-	std::vector<std::vector<std::vector<mpf_class> > > generate_epsilon(std::vector<int>& obs_seq, std::vector<std::vector<mpf_class> >& alpha, std::vector<std::vector<mpf_class> >& beta, std::vector<mpf_class>& scale_factors);
+	std::vector<std::vector<std::vector<mpfr::mpreal> > > generate_epsilon(std::vector<int>& obs_seq, std::vector<std::vector<mpfr::mpreal> >& alpha, std::vector<std::vector<mpfr::mpreal> >& beta, std::vector<mpfr::mpreal>& scale_factors);
 
 	void baum_welch(std::vector<int>& obs_seq);
 
--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# threat_detection
+# Hidden Markov Models and Computation with Words in Threat Detection Systems
+
+## Dependencies
+This project has several dependencies, which are listed below:
+* GNU multiple precision arithmetic library (GMP)
+```
+brew install gmp
+```
+* GNU multiple precision floating-point reliable library (MPFR)
+```
+brew install mpfr
+```
+* MPFR C++: [download link](http://www.holoborodko.com/pavel/mpfr/#download)
+	* copy mpreal.h into working directory
+## Compiling Instructions
+Compile:
+```
+g++ hmm.cpp -o hmm -std=c++11 -lmpfr -lgmp -lgmpxx
+```
+Execute:
+```
+./hmm
+```
